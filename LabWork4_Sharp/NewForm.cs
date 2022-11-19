128dd27738e8656b992985854483dad1269f876f
Inohara <irisha1234517@gmail.com>
2022-11-19 18:02:23 +0400

    добавление копирования

--- LabWork4_Sharp/NewForm.cs
+++ LabWork4_Sharp/NewForm.cs
@@ -18,7 +18,8 @@ namespace LabWork4_Sharp
 
 
         List<Item> list;
-        TreeNode selectedNode;
+        List<Item> copy;
+        TreeNode selected;
 
         public NewForm(int size)
         {
@@ -54,61 +55,36 @@ namespace LabWork4_Sharp
         {
             Random rnd = new();
             int kolCells = (int)numericUpDown.Value; //кол-во ячеек
-                                                     
-            if (kolCells < (_size - _occupied)) //проверка хватает ли места
-            {
-                if(Tree.Nodes.Count == 0 || Tree.SelectedNode != null)
-                {
-                    string newNodeStr = textBoxCreate.Text; //название node'а берем из текст бокса
-                    TreeNode newNode = new(newNodeStr); //создаем новый node 
 
-                    Item newItem = new(newNode, kolCells); //создаем новый элемент для листа
-
-                    int i;
-                    do 
-                    {
-                        i = rnd.Next(_cells.arr.Length); //индекс первого кластера
-                    } while (_cells.arr[i] != -2);
-
-                    newItem.cluster = i; //запоминаем индекс первого кластера в новом элементе листа
-                    _cells.arr[i] = -1; // отмечаем кластер как занятый
-                    kolCells--;
-
-                    //если у Node'а нексолько ячеек (начало)
-                    int temp = newItem.cluster;
-                    while (kolCells > 0)
-                    {
-                        i = rnd.Next(_cells.arr.Length);
-                        if (_cells.arr[i] == -2)
-                        {
-                            _cells.arr[temp] = i;
-                            _cells.arr[i] = -1;
-                            kolCells--;
-                            temp = i;
-                        }
-                    }
-                    //(конец)
-
-                    if (Tree.Nodes.Count == 0)
-                    {
-                        Tree.Nodes.Add(newNode);
-                        _occupied += kolCells;
-                        list.Add(newItem);
-                    }
-                    else if (Tree.SelectedNode != null)
-                    {
-                        Tree.SelectedNode.Nodes.Add(newNode);
-                        _occupied += kolCells;
-                        Tree.SelectedNode.ExpandAll(); //раскрываем все Node
-                        list.Add(newItem);
-                    }                   
-                }
-                Draw(-1);
-            }
-            else
+            if (kolCells > (_size - _occupied)) //если места не хватает
             {
                 MessageBox.Show("Недостаточно места", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }           
+                return; 
+            }
+
+            if (Tree.Nodes.Count == 0 || Tree.SelectedNode != null)
+            {
+                string newNodeStr = textBoxCreate.Text; //название node'а берем из текст бокса
+                TreeNode newNode = new(newNodeStr); //создаем новый node 
+
+                Item newItem = new(newNode, kolCells); //создаем новый элемент для листа
+                method(newItem);
+
+                if (Tree.Nodes.Count == 0)
+                {
+                    Tree.Nodes.Add(newNode);
+                    _occupied += kolCells;
+                    list.Add(newItem);
+                }
+                else if (Tree.SelectedNode != null)
+                {
+                    Tree.SelectedNode.Nodes.Add(newNode);
+                    _occupied += kolCells;
+                    Tree.SelectedNode.ExpandAll(); //раскрываем все Node
+                    list.Add(newItem);
+                }                   
+                Draw(-1);
+            }         
         }
 
 
@@ -141,8 +117,8 @@ namespace LabWork4_Sharp
         //выбор перетаскиваемого Node'а
         private void buttonNodeMove_Click(object sender, EventArgs e)
         {           
-            selectedNode = Tree.SelectedNode; //В selectedNode заносим выбранный Node
-            labelNodeMove.Text = selectedNode.Text; //в лейбл заносим имя выбранного 
+            selected = Tree.SelectedNode; //В selectedNode заносим выбранный Node
+            labelNodeMove.Text = selected.Text; //в лейбл заносим имя выбранного 
             Tree.SelectedNode.Remove(); // убираем выбранный Node
             Draw(-1);
         }
@@ -151,7 +127,7 @@ namespace LabWork4_Sharp
         //выбор нового места и перемещение
         private void buttonMoveToNode_Click(object sender, EventArgs e)
         {
-            Tree.SelectedNode.Nodes.Add(selectedNode); //добавляем выбранный в новое место
+            Tree.SelectedNode.Nodes.Add(selected); //добавляем выбранный в новое место
             labelMoveTo.Text = Tree.SelectedNode.Text; //в лейбл заносим имя нового места
             Draw(-1);
         }
@@ -160,9 +136,13 @@ namespace LabWork4_Sharp
         //выбор копируемого Node
         private void buttonNodeCopy_Click(object sender, EventArgs e)
         {
-            TreeNode copied = Tree.SelectedNode;
-            selectedNode = copied;
-            labelNodeCopy.Text = copied.Text;
+            selected = Tree.SelectedNode;
+            if (!CheckForCopy(selected))
+            {
+                selected = null;
+                return;
+            }
+            labelNodeCopy.Text = selected.Text;
             Draw(-1);
         }
 
@@ -170,9 +150,88 @@ namespace LabWork4_Sharp
         //выбор нового места копируемого элемента и копирование
         private void buttonToNode_Click(object sender, EventArgs e)
         {
-            Tree.SelectedNode.Nodes.Add(selectedNode);
-            labelCopyTo.Text = selectedNode.Text;
+            if (selected == null) return;
+
+            labelCopyTo.Text = Tree.SelectedNode.Text;
+            TreeNode clonedNode = (TreeNode)selected.Clone(); //клонировали узел и все его дочерние элементы
+            Tree.Nodes.Add(clonedNode);
+
+            Item temp = findNodeInList(selected); //находим выбранный для копирования в листе
+            Item cloned = new(temp._treeNode, temp.KolCells); // создаем новый элемент
+            method(temp); //выделяем ячейки для нового элемента
+            list.Add(cloned); //добавляем в лист новый элемент
+
+            foreach (TreeNode node in selected.Nodes) 
+            {
+                temp = findNodeInList(node);
+                cloned = new(temp._treeNode, temp.KolCells);
+                method(temp);
+                list.Add(cloned);
+            }
+            
             Draw(-1);
+        }
+
+        //подбирает и заполняет ячейки
+        private void method(Item item)
+        {
+            Random rnd = new();
+            int i, temp;
+            int cells = item.KolCells;
+
+            do
+            {
+                i = rnd.Next(_cells.arr.Length); //индекс первого кластера
+            } while (_cells.arr[i] != -2);
+
+            item.cluster = i; //запоминаем индекс первого кластера в новом элементе листа
+            _cells.arr[i] = -1; // отмечаем кластер как занятый
+            cells--;
+
+            temp = item.cluster;
+            while (cells > 0)
+            {
+                i = rnd.Next(_cells.arr.Length);
+                if (_cells.arr[i] == -2)
+                {
+                    _cells.arr[temp] = i;
+                    _cells.arr[i] = -1;
+                    cells--;
+                    temp = i;
+                }
+            }
+        }
+
+        //находит передаваемый узел в листе
+        private Item findNodeInList(TreeNode node)
+        {
+            foreach (Item temp in list)
+            {
+                if (temp._treeNode == node) return temp;
+            }
+            return null;
+        }
+
+        //проверяет хватит ли пустых ячеек при копировании
+        private bool CheckForCopy(TreeNode mainNode)
+        {
+            int needed = 0;
+            foreach(TreeNode temp in mainNode.Nodes)
+            {
+                for (int i = 0; i < list.Count; i++)
+                {
+                    if (list[i]._treeNode == temp || list[i]._treeNode == mainNode)
+                    {
+                        needed += list[i].KolCells;
+                        copy.Add(list[i]);
+                    }
+                }
+            }
+
+            if(needed <= (_size - _occupied)){
+                return true;
+            }
+            return false;
         }
     }
 }
